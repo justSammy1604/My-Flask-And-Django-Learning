ce1a75cfbeb21fbfcfab08ca48873a0a836526ff
Samuel Mesquita <100987969+justSammy1604@users.noreply.github.com>
2024-01-24 15:41:58 +0530

    Flask_updated

---
+++ ML_Flask/ML_Flask.py
@@ -0,0 +1,26 @@
+from flask import Flask,render_template,url_for,request,jsonify
+import numpy as np
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('model.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    init_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(init_features)]
+
+    prediction = model.predict(final_features)
+
+
+
+
+    return render_template('model.html',prediction_text='Prediction Class: {} '.format(prediction))
+
+
+if __name__ == '__main__':
+    app.run(debug=True,port=4444)
