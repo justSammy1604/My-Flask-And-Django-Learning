85df2a256282154c118c72d987cba3d1d250746f
Samuel Mesquita <100987969+justSammy1604@users.noreply.github.com>
2024-11-21 06:20:28 +0530

    Update main.py

--- meme-joke-flask/main.py
+++ meme-joke-flask/main.py
@@ -8,7 +8,7 @@ def get_meme():
     # sr = "/wholesomememes"
     # url = "<https://meme-api.herokuapp.com/gimme>" + sr
     url = "https://meme-api.com/gimme"
-    response = json.loads(requests.request("GET", url).text)
+    response = json.loads(requests.request("GET", url).text) 
     meme_large = response['preview'][0]
     subreddit = response['subreddit']
     return meme_large, subreddit
